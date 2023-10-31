bfbc1e327419809c7c88a7268f55d572e89b4c7f
hooonzaa <145441106+hooonzaa@users.noreply.github.com>
2023-10-31 13:10:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function SumaChecker() {
+
+    let suma = parseInt(document.getElementById("sumaInput").value)
+
+    let output = 0
+
+    for (let i = 0; i <= suma; i++) {
+
+        output = output + i
+
+        
+ }
+
+      
+
+ document.getElementById('Neo').innerHTML  =  output
+
+}
